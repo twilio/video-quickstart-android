3f8f066a9e1b65067351592b8096096f25db7557
Kumkum Bagchi <kbagchi@twilio.com>
2020-10-08 11:06:39 -0700

    Converted Dialog class to kotlin

---
+++ exampleCustomAudioDevice/src/main/java/com/twilio/video/examples/examplecustomaudiodevice/dialog/Dialog.kt
@@ -0,0 +1,29 @@
+package com.twilio.video.examples.examplecustomaudiodevice.dialog
+
+import android.R
+import android.content.Context
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
+import android.widget.EditText
+
+object Dialog {
+    fun createConnectDialog(participantEditText: EditText,
+                            callParticipantsClickListener: DialogInterface.OnClickListener?,
+                            cancelClickListener: DialogInterface.OnClickListener?,
+                            context: Context?): AlertDialog {
+        val alertDialogBuilder = AlertDialog.Builder(context!!)
+        alertDialogBuilder.setIcon(R.drawable.sym_call_outgoing)
+        alertDialogBuilder.setTitle("Connect to a room")
+        alertDialogBuilder.setPositiveButton("Connect", callParticipantsClickListener)
+        alertDialogBuilder.setNegativeButton("Cancel", cancelClickListener)
+        alertDialogBuilder.setCancelable(false)
+        setRoomNameFieldInDialog(participantEditText, alertDialogBuilder)
+        return alertDialogBuilder.create()
+    }
+
+    private fun setRoomNameFieldInDialog(roomNameEditText: EditText,
+                                         alertDialogBuilder: AlertDialog.Builder) {
+        roomNameEditText.hint = "room name"
+        alertDialogBuilder.setView(roomNameEditText)
+    }
+}
